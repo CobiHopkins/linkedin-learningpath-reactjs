6f93b0a80c6d30ffa16a2e9e97ae7fdeccb84551
Cobi Hopkins <cobihopkins@yahoo.com>
2024-10-23 16:48:23 +0100

    Completing React Essentials 6.3 - Handling loading states in Asynchronous React.

--- src/App.js
+++ src/App.js
@@ -15,19 +15,40 @@ function GithubUser({ name, location, avatar }) {
 function App() {
 
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/CobiHopkins`)
       .then(response => response.json())
-      .then(data => setData(data));
+      .then(data => setData(data))
+      .then(() => setLoading(false))
+      .catch(error => setError(error));
   }, []);
 
-  if(data) 
-    return (
-      <GithubUser name={data.name} location={data.location} avatar={data.avatar_url}/>
-    );
+  /*
+   * During asynchronous processes, you need to handle:
+   * - Loading state: Show a loading spinner or message.
+   * - Success state: Show the result of the process.
+   * - Error state: Show an error spinner or message.
+  */
 
-  return <h1>Data</h1>;
+  if(loading)
+    return <h1>Loading...</h1>;
+  if(error)
+    return <pre>{JSON.stringify(error)}</pre>;
+  if(!data)
+    return null;
+
+  return (
+    <GithubUser 
+      name={data.name} 
+      location={data.location} 
+      avatar={data.avatar_url}
+      />
+  );
+
 }
 
 export default App;
