4db1cdda67641b62a90c921662778d622b90bcb9
Cobi Hopkins <cobihopkins@yahoo.com>
2024-10-23 15:31:38 +0100

    Displaying data from an API using GitHub api.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
+function GithubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name}/>
+    </div>
+  )
+}
+
 function App() {
 
   const [data, setData] = useState(null);
@@ -14,7 +24,7 @@ function App() {
 
   if(data) 
     return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <GithubUser name={data.name} location={data.location} avatar={data.avatar_url}/>
     );
 
   return <h1>Data</h1>;
