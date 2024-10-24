df7ae57ac9a1c1e21c61950a6b08136de1b6ce85
Cobi Hopkins <cobihopkins@yahoo.com>
2024-10-24 13:56:31 +0100

    Added react-router-dom v6. Added material-ui. Created home/about/contact components. Created routes for each. Completed essentials-7.1 and essentials-7.2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
-
-const tahoe_peaks = [
-  { name: "Freel", elevation: 10891 },
-  { name: "Monument", elevation: 10067 },
-  { name: "Pyramid", elevation: 9983 },
-  { name: "tallac", elevation: 9735 }
-];
-
-function List({ data, renderItem, renderEmpty }) {
-  return !data.length ? renderEmpty : <ul>{data.map((item) => (
-    <li key={item.name}>{renderItem(item)}</li>
-  ))}</ul>
-}
+import Home from './components/home';
 
 function App() {
 
   return (
-    <List data={tahoe_peaks} renderEmpty={<p>This list is empty.</p>} renderItem={item => 
-      <>
-        {item.name} - {item.elevation} ft.
-      </>}
-    />
+    <Home />
   );
 
 }
