3c50210b119fed6c16e42dcdb27d69666a428d9b
Cobi Hopkins <cobihopkins@yahoo.com>
2024-11-12 15:12:13 +0000

    Completed 2.0, 3.0 in a single commit. Created a Search component, a Sorting feature (without functionality), added a list of appointments via a data.json file. Added data.json file to src (needs moving to public.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
+import appointmentList from './data.json';
+import Search from './components/Search/Search';
+import AddAppointment from './components/AddAppointment/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo/AppointmentInfo';
 
-import { BiArchive } from 'react-icons/bi';
-
+import { BiCalendar } from'react-icons/bi';
 
 function App() {
 
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top"/> Your Appointments
+      <h1 className="text-5xl mb-3">
+        <BiCalendar className="inline-block text-red-400 align-top"/> Your Appointments
       </h1>
+      <AddAppointment />
+      <Search />
+
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map(appointment => (
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
+        ))}
+      </ul>
     </div>
   );
 
